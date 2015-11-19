ab4ed39d870cd48d1979a6f2166fda94464177aa
Augusto Soares <gutosoares@outlook.com>
2015-11-19 10:34:54 -0200

    Criação das variaveis iniciais HS

---
+++ js/harsam.js
@@ -0,0 +1,74 @@
+// Método de Makkink
+
+function makkink() {
+    // validação da variaveis de entrada
+    if(document.getElementById("dia").value == "") {
+        alert("Entre com o dia desejado.");
+        return false;
+    }
+
+    if(document.getElementById("dia").value < 1 || (document.getElementById("dia").value > 31)) {
+        alert("Valor inválido!");
+        return false;
+    }
+
+    if(document.getElementById("mes").value == "") {
+        alert("Entre com o mês desejado.");
+        return false;
+    }
+
+    if(document.getElementById("mes").value == 2 && (document.getElementById("dia").value > 29)) {
+        alert("O valor do dia informado para o mês de Fevereiro está incorreto.");
+        return false;
+    }
+
+    if(document.getElementById("mes").value < 1 || (document.getElementById("mes").value > 12)) {
+        alert("Valor inválido!");
+        return false;
+    }
+
+    if(document.getElementById("ano").value == "") {
+        alert("Entre com o ano desejado.");
+        return false;
+    }
+
+    if(document.getElementById("temp1").value == "") {
+        alert("Entre com o valor da temperatura as 9h00.");
+        return false;
+    }
+
+    if(document.getElementById("temp2").value == "") {
+        alert("Entre com o valor da temperatura as 21h00.");
+        return false;
+    }
+
+    if(document.getElementById("tempMax").value == "") {
+        alert("Entre com o valor da temperatura máxima.");
+        return false;
+    }
+
+    if(document.getElementById("tempMin").value == "") {
+        alert("Entre com o valor da temperatura mínima.");
+        return false;
+    }
+
+    if(document.getElementById("latitude-graus").value == "") {
+        alert("Entre com o valor da latitude em graus.");
+        return false;
+    }
+
+    if(document.getElementById("latitude-min").value == "") {
+        alert("Entre com o valor da latitude em minutos.");
+        return false;
+    }
+
+    // criando as variaveis iniciais
+	var dia = parseFloat(document.getElementById('dia').value);
+	var mes = parseFloat(document.getElementById('mes').value);
+	var ano = parseFloat(document.getElementById('ano').value);
+	var temp1 = parseFloat(document.getElementById('temp1').value);
+	var temp2 = parseFloat(document.getElementById('temp2').value);
+	var tempMax = parseFloat(document.getElementById('tempMax').value);
+	var tempMin = parseFloat(document.getElementById('tempMin').value);
+    var latGraus = parseFloat(document.getElementById('latitude-graus').value);
+	var latMin = parseFloat(document.getElementById('latitude-min').value);
